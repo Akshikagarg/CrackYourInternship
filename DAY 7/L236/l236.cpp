62b813844aa70d1ea5917e10ad2a9c30eabcec80
Akshikagarg <105767216+Akshikagarg@users.noreply.github.com>
2022-06-05 14:28:12 +0530

    Create l236.cpp

---
+++ DAY 7/L236/l236.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root->val==p->val or root->val==q->val)
+            return root;
+        if(!root->left and !root->right)
+            return NULL;
+        
+        TreeNode *left=NULL,*right=NULL;
+        if(root->left)
+            left = lowestCommonAncestor(root->left,p,q);
+        if(root->right)
+            right = lowestCommonAncestor(root->right,p,q);
+        if(left and right)
+            return root;
+        return left==NULL?right:left;
+    }
+};
