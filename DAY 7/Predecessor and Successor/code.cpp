8018d8584ff7a61a5e19f2b209209be64d12a9f3
Akshikagarg <105767216+Akshikagarg@users.noreply.github.com>
2022-06-05 13:37:37 +0530

    Create code.cpp

---
+++ DAY 7/Predecessor and Successor/code.cpp
@@ -0,0 +1,17 @@
+void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
+{
+    if(root == NULL)
+        return;
+    
+    findPreSuc(root->left, pre, suc, key);
+    
+    if(root && root->key < key)
+        pre = root;
+    else if(root && root->key > key)
+    {
+        if(suc == NULL )
+            suc = root;
+    }
+    
+    findPreSuc(root->right, pre, suc, key);
+}
