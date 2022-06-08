548d74fdaa9c2d528e63d90fae0b305a078e0659
Akshikagarg <105767216+Akshikagarg@users.noreply.github.com>
2022-06-08 09:43:52 +0530

    Create l103.cpp

---
+++ DAY 10/L103/l103.cpp
@@ -0,0 +1,33 @@
+class Solution{
+    
+public:
+vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+    vector<vector<int>> res;
+    if(!root) return res;
+    std::deque<TreeNode*> deq;
+    deq.push_back(root);
+    int iszig=1;
+    while(!deq.empty()) {
+        int sz=deq.size();
+        iszig=iszig^1;
+        vector<int> row;
+        while(sz--) {
+            if(iszig) { // pop_front, push_back, right then left
+                root=deq.front();deq.pop_front();
+                row.push_back(root->val);
+                if(root->right) deq.push_back(root->right);
+                if(root->left) deq.push_back(root->left);
+            }
+            else { // pop_back, push_front, left then right
+                root=deq.back();deq.pop_back();
+                row.push_back(root->val);
+                if(root->left) deq.push_front(root->left);
+                if(root->right) deq.push_front(root->right);
+            }
+        }
+        res.push_back(row);
+    }
+    return res;
+}
+};
+
