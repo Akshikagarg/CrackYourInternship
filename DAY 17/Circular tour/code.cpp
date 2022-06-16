06a390255f17a5c17fa9986762596d0fca3df34e
Akshikagarg <105767216+Akshikagarg@users.noreply.github.com>
2022-06-16 13:20:27 +0530

    Create code.cpp

---
+++ DAY 17/Circular tour/code.cpp
@@ -0,0 +1,22 @@
+class Solution{
+  public:
+  
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+       //Your code here
+         int totalSum = 0 , currentSum = 0 , j = 0 ;
+      
+      for(int i = 0 ; i < n ; i++){
+          totalSum += p[i].petrol - p[i].distance ;
+          currentSum += p[i].petrol - p[i].distance ;
+          
+          if(currentSum < 0 ) {
+              j = i + 1 ;
+              currentSum = 0 ;
+          }
+      }
+      return totalSum < 0 ? -1 : j ;
+    }
+};
