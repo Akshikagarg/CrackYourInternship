d9961e9c4eec242e684232759554b2abcc533f7c
Akshikagarg <105767216+Akshikagarg@users.noreply.github.com>
2022-06-27 20:31:51 +0530

    Update l778.cpp

--- DAY 25/L778/l778.cpp
+++ DAY 25/L778/l778.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size(), ans = max(grid[0][0], grid[n-1][n-1]);
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        vector<vector<int>> visited(n, vector<int>(n, 0));
-        visited[0][0] = 1;
-        vector<int> dir({-1, 0, 1, 0, -1});
-        pq.push({ans, 0, 0});
-        while (!pq.empty()) {
-            auto cur = pq.top();
+    vector<int> DIR = {1, 0, -1, 0, 1};
+    int n, result = 0;
+    int swimInWater(vector<vector<int>>& g) {
+        n = g.size();
+        vector<vector<bool>> vis(n, vector<bool>(n));
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        pq.push({g[0][0], {0, 0}});
+        while(!pq.empty()){
+            pair<int, pair<int, int>> A = pq.top();
             pq.pop();
-            ans = max(ans, cur[0]);
-            for (int i = 0; i < 4; ++i) 
-            {
-                int r = cur[1] + dir[i], c = cur[2] + dir[i+1];
-                if (r >= 0 && r < n && c >= 0 && c < n && visited[r][c] == 0) {
-                    if (r == n-1 && c == n-1) return ans;
-                    pq.push({grid[r][c], r, c});
-                    visited[r][c] = 1;
-                }
+            int cost = A.first, r = A.second.first, c = A.second.second;
+            result = max(result, cost);
+            if(r == n - 1 && c == n - 1) return result;
+            for(int k = 0; k < 4; k++){
+                int newR = r + DIR[k], newC = c + DIR[k + 1];
+                if(newR == n || newC == n || newC < 0 || newR < 0 || vis[newR][newC])
+                    continue;
+                vis[newR][newC] = 1;
+                pq.push({g[newR][newC], {newR, newC}});
             }
         }
-        return -1;
+        return result;
     }
 };
